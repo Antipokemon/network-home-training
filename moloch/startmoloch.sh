92881aea51a1b6f63c11f70fbe823181f7c51b82
Andrew Campbell <andrew.t.campbell35mil@mail.mil>
2020-12-04 14:00:02 -0600

    Fixes to run

--- moloch/startmoloch.sh
+++ moloch/startmoloch.sh
@@ -8,7 +8,7 @@ sleep 10
 
 if $value==0
   # intialize moloch
-  echo INIT | /data/moloch/db/db.pl http://elasticsearch:9200 init
+  echo INIT | /data/moloch/db/db.pl http://localhost:9200 init
   /data/moloch/bin/moloch_add_user.sh admin "Admin User" password --admin
   echo "1" > /init.txt
 fi
