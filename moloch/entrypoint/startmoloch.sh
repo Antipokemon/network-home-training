ac85c2b96bca4b8e5450191633d6af9ebd4ee902
Andrew Campbell <andrew.t.campbell35mil@mail.mil>
2020-12-09 13:48:13 -0600

    moloch container fixes

--- moloch/entrypoint/startmoloch.sh
+++ moloch/entrypoint/startmoloch.sh
@@ -7,19 +7,23 @@ capture=$(cat /capture.txt)
 echo "Giving ES time to start..."
 sleep 10
 
-if $init==0
+if (($init==0))
+then
   # intialize moloch
   echo INIT | /data/moloch/db/db.pl http://localhost:9200 init
   /data/moloch/bin/moloch_add_user.sh admin "Admin User" password --admin
   echo "1" > /init.txt
 fi
 
-if $capture==TRUE
-  echo "Starting moloch-capture on default interface."
-  /bin/bash -c "/data/moloch/bin/moloch-capture -c /data/moloch/etc/config.ini --host $HOSTNAME >> /data/moloch/logs/capture.log 2>&1 &"
-fi
-
+# Start Viewer
 echo "Starting moloch-viewer"
 cd /data/moloch/viewer
 /bin/bash -c "/data/moloch/bin/node viewer.js -h"
 /bin/bash -c "/data/moloch/bin/node viewer.js -c /data/moloch/etc/config.ini >> /data/moloch/logs/viewer.log 2>&1"
+
+if (($capture==TRUE))
+then
+  #Start Capture
+  echo "Starting moloch-capture on default interface."
+  /bin/bash -c "/data/moloch/bin/moloch-capture -c /data/moloch/etc/config.ini --host $HOSTNAME >> /data/moloch/logs/capture.log 2>&1 &"
+fi
